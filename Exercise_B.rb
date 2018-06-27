fe177637022d94960d212bbed24a54b188c4a2a0
Digory Philbrow <40001296+Digory@users.noreply.github.com>
2018-06-27 18:39:11 +0100

    Exercise_B complete

--- Exercise_B.rb
+++ Exercise_B.rb
@@ -73,21 +73,30 @@ end
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 p users["Jonathan"][:twitter]
+
 # 2. Get Erik's hometown
 p users["Erik"][:home_town]
+
 # 3. Get the array of Erik's lottery numbers
 p users["Erik"][:lottery_numbers]
+
 # 4. Get the type of Avril's pet Monty
 p find_pet_species(users["Avril"][:pets], "monty")
+
 # 5. Get the smallest of Erik's lottery numbers
 p users["Erik"][:lottery_numbers].min
+
 # 6. Return an array of Avril's lottery numbers that are even
 p find_even_numbers(users["Avril"][:lottery_numbers])
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"][:lottery_numbers] << 7
+
 # 8. Change Erik's hometown to Edinburgh
 users["Erik"][:home_town] = "Edinburgh"
+
 # 9. Add a pet dog to Erik called "Fluffy"
 users["Erik"][:pets] << {name: "Fluffy", species: "dog"}
+
 # 10. Add another person to the users hash
 users["Digory"] = {home_town: "East Calder", pets: [{name: "Bandit", species: "cat"}]}
